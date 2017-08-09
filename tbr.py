907da4e4846dc4f1f84d71d81fc187223c160f0e
sixthextinction <prithwish.nath@gmail.com>
2017-08-10 00:49:37 +0530

    added side menu on hover

--- tbr.py
+++ tbr.py
@@ -6,32 +6,83 @@
 #   2) class names start in caps
 
 # Todo : 
-#   scrollbar should move down automatically when an entry goes past initial window
-#   prevent duplicate entries (have Name and Author together be primary keys?)
 #   try/catch all SQL
 #   hover over book name to see cover and description grabbed from internet,
-#   click to go to goodreads link in system default browser
-#   order SQL-retrieved results by something
+#   order SQL-retrieved results by date added
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
 from Tkinter import *
+import webbrowser
 import tkMessageBox
 import sqlite3
+import re
 import urllib2
 import xml.etree.ElementTree as ET
+from PIL import Image, ImageTk
+
+root = Tk()
+hover_window = Toplevel(root)
+hover_window.state('withdrawn')
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # our custom Label class, inheriting from the base Tkinter Label class
 class BookNameLabel(Label):
-    def __init__(self, master, url, *args, **kwargs):           # wont work if master isnt included
+    def __init__(self, master, book_url, author_url, image_url, *args, **kwargs):           # wont work if master isnt included
         Label.__init__(self, master, *args, **kwargs)           # if master isnt included, all new labels will go to a weird phantom zone between the two frames
         # self.master = master
-        self.url = url
+        self.book_name  = self['text'].split(' by ')[0]
+        self.author_name  = self['text'].split(' by ')[1]
+        self.book_url   = book_url
+        self.author_url = author_url
+        self.image_url  = image_url
         self.bind("<Enter>", self.onHover)
+
+
+    # hover on list labels callback
     def onHover(self,event):
-        pass
-        #TODO : popup window with pictures and stuff
-        # print "TEST: Mouse hovering over %r" % event.widget
-        # print "URL =  %r" % event.widget.url
+        # fetch global var hover_window and destroy it, to avoid duplicate hover_windows for what were gonna do
+        global hover_window
+        hover_window.destroy()
+
+        # then create a new one
+        global root
+        hover_window = Toplevel(root)
+        offset_x = root.winfo_rootx() + root.winfo_width()
+        offset_y = root.winfo_rooty()
+        hover_window.geometry("+%d+%d" % (offset_x, offset_y - 31)) # x always off by 8 pixels, y a;lways off by 30-32. Dont know why.
+
+        # create two frames for this new window
+        frame_coverart  = Frame(hover_window)
+        frame_details   = Frame(hover_window)
+        # pack em
+        frame_coverart.grid(row = 0, column = 0)
+        frame_details.grid(row = 0, column = 1)
+
+        # grab cover image and turn it into a label
+        image_data      = urllib2.urlopen(self.image_url)
+        image           = Image.open(image_data)
+        image.thumbnail((512,512))
+        image_for_tk    = ImageTk.PhotoImage(image)
+        img_label       = Label(frame_coverart, image = image_for_tk)
+        img_label.img   = image_for_tk       
+
+        # make hyperlink labels out of the rest of the data
+        label_book_name     = Label(frame_details, text = self.book_name, fg = 'blue', cursor = 'hand2')
+        label_author_name   = Label(frame_details, text = self.author_name, fg = 'blue', cursor = 'hand2')
+        #book_description will go here too once scraped
+
+        # pack em all to finish this window off
+        label_book_name.grid()
+        label_author_name.grid()
+        img_label.grid()
+
+        # set up the link label callbacks
+        label_book_name.bind("<Button-1>", lambda event : self.openLink(event, self.book_url))
+        label_author_name.bind("<Button-1>", lambda event : self.openLink(event, self.author_url))
+
+    # callback for link labels
+    def openLink(self, event, url):
+        webbrowser.open_new(url)
+
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class App:
 
@@ -107,6 +158,8 @@ class App:
         self.bookEntryField.bind("<Button-1>", self.clearTextField)         # clear book entry field when textfield clicked with default text in it
         self.authorEntryField.bind("<Button-1>", self.clearTextField)       # clear author entry field when textfield clicked with default text in it
         #self.master.bind_class("Label", "<Enter>", self.onHover)           # already done in custom Label class
+        self.listCanvas.bind("<Enter>", self.killHoverWindow)               # destroy hover_window if mouse pointer enters anywhere but labels
+        self.entryFrame.bind("<Enter>", self.killHoverWindow)               # destroy hover_window if mouse pointer enters anywhere but labels
 
 
         # db stuff
@@ -114,15 +167,20 @@ class App:
         self.db_conn.text_factory = str #for ryu murakami and utf-8 characters?
         self.db_cursor = self.db_conn.cursor()
         self.db_conn.execute(
-            "CREATE TABLE IF NOT EXISTS Tbr (Name TEXT NOT NULL, Author TEXT NOT NULL, PRIMARY KEY (Name, Author))")
-        self.db_cursor.execute("SELECT Name, Author FROM Tbr")
-        # name = row[0], author = row[1]
+            "CREATE TABLE IF NOT EXISTS Tbr (BookName TEXT NOT NULL, BookURL TEXT, AuthorName TEXT NOT NULL, AuthorURL TEXT, ImageURL TEXT, PRIMARY KEY (BookName, AuthorName))")
+        self.db_cursor.execute("SELECT BookName, BookURL, AuthorName, AuthorURL, ImageURL FROM Tbr")
+        # name = row[0], url = row[1], and so on
+        # for row in self.db_cursor:
+        #     self.books_authors[row[0]] = row[1]
+        # for item in self.books_authors:
         for row in self.db_cursor:
-            self.books_authors[row[0]] = row[1]
-        for item in self.books_authors:
-            labelText = "%s by %s" % (item, self.books_authors[item])  # when deleted, delete from database by splitting text along " by "
+            # labelText = "%s by %s" % (item, self.books_authors[item])  # when deleted, delete from database by splitting text along " by "
+            label_book_and_authorname   = "%s by %s" % (row[0], row[2])
+            label_book_url              = row[1]
+            label_author_url            = row[3]
+            label_image_url             = row[4]
             # create labels out of these
-            self.createLabel(labelText)
+            self.createLabel(label_book_and_authorname, label_book_url, label_author_url, label_image_url)
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -151,28 +209,49 @@ class App:
         book_results = root.findall('./search/results/work')
 
         for result in book_results: # WARNING : later sections will cause issues if there are no results returned here i.e. this loop skipped
-            book_id         =   result.find('best_book/id')
+            book_id         =   result.find('best_book/id').text
             title           =   result.find('best_book/title').text.encode("utf-8")             # need explicit encoding for results with non-unicode characters e.g ryu murakami
             author_id       =   result.find('best_book/author/id').text
             author          =   result.find('best_book/author/name').text.encode("utf-8")       # need explicit encoding for results with non-unicode characters e.g ryu murakami
             image_url       =   result.find('best_book/image_url').text
 
+            regex_title = r'\w*{}\w*'.format(q_title)
+            regex_author = r'\w*{}\w*'.format(q_author)
+
             # if author == "William Gibson" and title == "Neuromancer":
-            if author.lower() == q_author.lower() and title.lower() == q_title.lower():
+            if author.lower() == q_author.lower() and title.lower() == q_title.lower(): #cant convert to lowercase for non unicode characters; will fail every time for those
                 print "[EXACT MATCH]"
                 print("[{}] {} - [{}] {}").format(book_id, title, author_id, author)
-                print("https://www.goodreads.com/book/show/{}").format(book_id)
+                # print("https://www.goodreads.com/book/show/{}").format(book_id)
                 book_url = "https://www.goodreads.com/book/show/{}".format(book_id) # <-- book url that gets passed. Same string as line above.
-                print("https://www.goodreads.com/author/show/{}").format(author_id)
-                print("IMAGE URL : {}").format(image_url)
+                print book_url
+                # print("https://www.goodreads.com/author/show/{}").format(author_id)
+                author_url = "https://www.goodreads.com/author/show/{}".format(author_id)
+                # print("IMAGE URL : {}").format(image_url)
                 break
+            # elif with re goes here
+            elif (re.findall(regex_title, title)) or (re.findall(regex_author,author)):
+                tkMessageBox.showwarning("Partial Match Found", "Using Best Match.")
+                print "[DISPLAYING BEST MATCH]"
+                print("[{}] {} - [{}] {}").format(book_id, title, author_id, author)
+                # print("https://www.goodreads.com/book/show/{}").format(book_id)
+                book_url = "https://www.goodreads.com/book/show/{}".format(book_id) # <-- book url that gets passed. Same string as line above.
+                print book_url
+                # print("https://www.goodreads.com/author/show/{}").format(author_id)
+                author_url = "https://www.goodreads.com/author/show/{}".format(author_id)
+                # print("IMAGE URL : {}").format(image_url)
+                break
+            # Edge cases (mostly due to goodreads screwing up /eyeroll)
             else:
-                tkMessageBox.showwarning("No Exact Match Found", "Using First Match")
+                tkMessageBox.showerror("No Match Of Note Found", "Using First Match.")
                 print "[DISPLAYING FIRST MATCH]"
                 print("[{}] {} - [{}] {}").format(book_id, title, author_id, author)
-                print("https://www.goodreads.com/book/show/{}").format(book_id)
-                print("https://www.goodreads.com/author/show/{}").format(author_id)
-                print("IMAGE URL : {}").format(image_url)
+                # print("https://www.goodreads.com/book/show/{}").format(book_id)
+                book_url = "https://www.goodreads.com/book/show/{}".format(book_id) # <-- book url that gets passed. Same string as line above.
+                print book_url
+                # print("https://www.goodreads.com/author/show/{}").format(author_id)
+                author_url = "https://www.goodreads.com/author/show/{}".format(author_id)
+                # print("IMAGE URL : {}").format(image_url)
                 break
         #-------------------------------------
         # if results WERE returned via API,
@@ -180,17 +259,20 @@ class App:
             # overwrite book_name and author_name with Goodreads API returns instead    
             book_name = title
             author_name = author
-        # if NO results were returned
+        # if NO results were returne
         else:
             # issue statement to console about it
-            print "WARNING : Goodreads API search for \'%s by %s\' returned no results." % (book_name, author_name)
-            print "Using entered values as is"
+            err_message = "WARNING : Goodreads API search for \'%s by %s\' returned no results." % (book_name, author_name)
+            err_message+= "\nUsing entered values as is."
+
+            tkMessageBox.showwarning("No results", err_message)
+
         # make a label only if both fields have nonzero lengths (no empty book/author names)
         if len(book_name) > 0 and len(author_name) > 0:
             label_text = book_name + " by " + author_name
-            self.createLabel(label_text) # pass over the book URL as well. CAN'T right now; add column for it in db first.
+            self.createLabel(label_text, book_url, author_url, image_url) # pass over the book URL as well. CAN'T right now; add column for it in db first.
             # SQL Query to add book_name and author_name to database
-            self.db_cursor.execute("INSERT INTO Tbr(Name, Author) VALUES (?, ?)", (book_name, author_name))  # do it this way instead of directly using raw strings to prevent SQL Injection attacks
+            self.db_cursor.execute("INSERT INTO Tbr(BookName, BookURL, AuthorName, AuthorURL, ImageURL) VALUES (?, ?, ?, ?, ?)", (book_name, book_url, author_name, author_url, image_url))  # do it this way instead of directly using raw strings to prevent SQL Injection attacks
             self.db_conn.commit()
         # clear out the two fields
         self.bookEntryField.delete(1.0, END)
@@ -211,7 +293,7 @@ class App:
             book_name = text.split(" by ")[0]
             author_name = text.split(" by ")[1]
             # sql query to delete this
-            self.db_cursor.execute("DELETE FROM Tbr WHERE Name = ? AND Author = ?", (book_name, author_name)) # do it this way instead of directly using raw strings to prevent SQL Injection attacks
+            self.db_cursor.execute("DELETE FROM Tbr WHERE BookName = ? AND AuthorName= ?", (book_name, author_name)) # do it this way instead of directly using raw strings to prevent SQL Injection attacks
             self.db_conn.commit()
             # remove the label widget itself
             event.widget.destroy()
@@ -268,16 +350,16 @@ class App:
                 scroll = 1
             self.listCanvas.yview_scroll(scroll * "units")
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    def createLabel(self, labelText, book_url= None): # cant add book url's yet; make a column for it in the database
+    def createLabel(self, labelText, book_url, author_url, image_url): # cant add book url's yet; make a column for it in the database
         # to alternate color schemes based on flag
         choice_of_style = self.pickColorScheme()
-        # add the label, pack it too
-        book_added = BookNameLabel(self.listFrame, url = book_url, text=labelText, font=('Courier', '14'), bg=choice_of_style["bg"],
+        # add the label. pass bookname, authorname, bookurl, authorurl, imageurl as well
+        book_added = BookNameLabel(self.listFrame, book_url = book_url, author_url = author_url, image_url = image_url, text=labelText, font=('Courier', '14'), bg=choice_of_style["bg"],
                            fg=choice_of_style["fg"], wraplength = 400)
         book_added.pack(side=TOP, fill=X)
         book_added.bind("<Button-1>", self.delete)
-        # store books
-        self.labels.append(book_added)  # append book_added if need to store in list as Label instances instead
+        # store books as MyLabel instances
+        self.labels.append(book_added)  
         # delete entry field after return is hit
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
     # on enter or tab on book textfield, shift focus to author textfield
@@ -285,8 +367,11 @@ class App:
         self.authorEntryField.focus_set()
         return "break"
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def killHoverWindow(self, event):
+        global hover_window
+        hover_window.destroy()
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-root = Tk()
 app = App(root)
 app.start()
 root.mainloop()
